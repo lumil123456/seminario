cb98494e57d74eae8b6483e341dfb40eef54179c
Lourdes <lumil@hotmail.com>
2014-09-04 20:39:14 -0500

    tercer commit

---
+++ c.py
@@ -0,0 +1,9 @@
+print("el inciso c del ejercicio 1")
+def suma(x, y):
+	return  x + y
+a=raw_input("ingrese el primer numero")
+b=raw_input("ingrese el segundo numero")
+value=int(a)
+value1=int(b)
+print suma(value, value1)
+print " "
